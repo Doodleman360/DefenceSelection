208eec4c9b2117b6af58051fdf9ef7bd61a7b429
Doodleman360 <julislo123@gmail.com>
2016-03-03 07:04:53 -0700

    Initial Commit

---
+++ SharkScout V.2/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SharkScout V.2
+//
+
+//  Copyright (c) 2016 Julien Sloan. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SharkScout V.2/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SharkScout V.2
+//
+
+//  Copyright (c) 2016 Julien Sloan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
